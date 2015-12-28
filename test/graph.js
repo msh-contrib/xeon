7da02e11a46c624561bb7d218b9fa1ef5a146afd
Oleh Kuchuk <kuchuklehjs@gmail.com>
2015-12-28 18:36:47 +0200

    clean, add proper indentation

--- test/graph.js
+++ test/graph.js
@@ -1,14 +1,33 @@
 var test = require('tape');
 var graph = require('../lib/graph');
+	
+var fixtures = {};
+fixtures.graph = new graph.Graph();
+fixtures.xgraph = graph.Graph();
 
-test('Graph should be properly created', function (t) {
+test('graph should be properly created', function (t) {
 	t.plan(2);
+	t.ok(fixtures.graph instanceof graph.Graph);
+	t.ok(
+		fixtures.xgraph instanceof graph.Graph, 
+		'should be instance of Graph even called without new'
+	);
+	t.end();
+});
 
-	var fixture = new graph.Graph();
-	var fixture_graph = graph.Graph();
+test('addNode should be defined', function (t) {
+	t.plan(1);
+	t.ok(typeof fixtures.graph.addNode === 'function', 'addNode should be a function');
+	t.end();
+});
 
-	t.ok(fixture instanceof graph.Graph);
-	t.ok(fixture_graph instanceof graph.Graph, 'should be instance of Graph even called without new');
-
+test('addNode should proper work', function (t) {
+	t.plan(3);
+	fixtures.graph.addNode('test_key');
+	t.ok(fixtures.graph._graph['test_key'], 'should add proper key');
+	t.throws(function () { fixtures.graph.addNode({}) }, 'should throw on incorrect input');
+	t.throws(function () { 
+		fixtures.graph.addNode('correct_key', 'str')}, 
+		'should throw when parameters not object');
 	t.end();
 });
