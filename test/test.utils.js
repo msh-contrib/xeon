af3a6622f37af2fb84db1dacb4d75a50d69546cb
Oleh Kuchuk <kuchuklehjs@gmail.com>
2016-02-24 00:56:21 +0200

    add test cases, decoupled, clean

---
+++ test/test.utils.js
@@ -0,0 +1,6 @@
+// var test = require('tape');
+// var utils = require('../lib/utils');
+
+// test('test', function (t) {
+// 	// test something
+// });
--- test/utils.js
+++
@@ -1,6 +0,0 @@
-var test = require('tape');
-var utils = require('../lib/utils');
-
-test('test', function (t) {
-	// test something
-});
