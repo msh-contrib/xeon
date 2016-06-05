913b5350a87e24ff46dbca68398a8c9eecbf9fa8
Oleh Kuchuk <kuchuklehjs@gmail.com>
2016-06-05 22:44:24 +0300

    added support for config file

--- dist/cli.js
+++ dist/cli.js
@@ -3,6 +3,10 @@
 
 'use strict';
 
+var _path = require('path');
+
+var _path2 = _interopRequireDefault(_path);
+
 var _package = require('../package.json');
 
 var _package2 = _interopRequireDefault(_package);
@@ -23,6 +27,14 @@ var _notifier = require('./notifier');
 
 var _notifier2 = _interopRequireDefault(_notifier);
 
+var _fs = require('fs');
+
+var _file = require('./file');
+
+var _prettyjson = require('prettyjson');
+
+var _prettyjson2 = _interopRequireDefault(_prettyjson);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 // notify if there are some updates
@@ -38,14 +50,31 @@ var cli = (0, _meow2.default)('\n    Usage\n      $ xeon -i ./index[.sh]\n\n    
   }
 });
 
-var config = {
-  entry: cli.flags.input,
-  output: cli.flags.output,
-  allowExternal: cli.flags.external
-};
+(function () {
+  function configsLookup() {
+    try {
+      // if there is no flags search for config file
+      if (!Object.keys(cli.flags).length) {
+        var configFilePath = _path2.default.join(process.cwd(), '.xeonconf.json');
+        var stats = (0, _fs.statSync)(configFilePath);
+        if (stats.isFile()) return JSON.parse((0, _file.readFile)(configFilePath));
+        throw new Error('Could not find config file');
+      }
+      // or return config from flags
+      return {
+        entry: cli.flags.input,
+        output: cli.flags.output,
+        allowExternal: cli.flags.external
+      };
+    } catch (error) {
+      console.log('Error while initing config object\n ' + _prettyjson2.default.render(error));
+    }
+  }
 
-(function (config) {
+  var config = configsLookup();
+
   if (!config.entry) return console.log('Entry file should be defined');
+
   var xeon = new _xeon2.default(config);
 
   (0, _log2.default)(xeon);
@@ -63,4 +92,4 @@ var config = {
       processBundle();
     });
   }
-})(config);
+})();
